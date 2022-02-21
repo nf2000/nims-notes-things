c754b67f736f46957d3ed551e8f96fe48abd4ace
nf2000 <nimraf@chillibean.com>
2022-02-21 09:14:52 +0000

    Added input date, name and note in form component

--- nims-notes-things/src/Form.js
+++ nims-notes-things/src/Form.js
@@ -1,4 +1,6 @@
 import TextArea from './TextArea';
+import Input from './Input';
+
 import React, { useState } from 'react';
 
 const Form = (props) =>{
@@ -22,6 +24,11 @@ const Form = (props) =>{
       alert("This field is required")
     }
   }
+  const handleOnBlurDate = e => {
+    if(formData.name === ''){
+      alert("This field is required")
+    }
+  }
   
   return (
     <div className="App">
@@ -32,7 +39,16 @@ const Form = (props) =>{
         onBlur={handleOnBlurNote}
         value={formData.name}
       />
-      <h5>Note: {formData.name}</h5>
+      <h5>Name: {formData.name}</h5>
+
+      <Input
+        name= "date"
+        placeholder= "Please enter the date"
+        onChange={(e)=> handleOnChange(e, "date")}
+        onBlur={handleOnBlurDate}
+        value={formData.date}
+      />
+      <h5>Date: {formData.date}</h5>
 
       <TextArea
         name= "note"

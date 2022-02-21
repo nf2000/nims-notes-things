e42d8e00499e6a74910ba927eec7fdededcdf5e8
nf2000 <nimraf@chillibean.com>
2022-02-21 10:32:24 +0000

    prettier format

--- nims-notes-things/src/App.js
+++ nims-notes-things/src/App.js
@@ -1,36 +1,36 @@
-import React, { useState } from 'react';
-import './App.css';
-import TextArea from './TextArea';
+import React, { useState } from "react";
+import "./App.css";
+import TextArea from "./TextArea";
 
 function App() {
-  const [formData, setFormData] = useState({note: ''});
+  const [formData, setFormData] = useState({ note: "" });
 
   const handleOnChange = (e, key) => {
     const newForm = {
       ...formData,
       [key]: e.target.value,
-    }
-   setFormData(newForm)
-  }
+    };
+    setFormData(newForm);
+  };
 
   const handleOnBlur = (e) => {
-    if(formData.note === ''){
-      alert("This field is required")
+    if (formData.note === "") {
+      alert("This field is required");
     }
-  }
-  
+  };
+
   return (
     <div className="App">
       <TextArea
-        name= "name"
+        name="name"
         placeholder="Please Enter Your Note"
-        onChange={(e)=> handleOnChange(e, "note")}
+        onChange={(e) => handleOnChange(e, "note")}
         onBlur={handleOnBlur}
         value={formData.note}
       />
-    <h5>Note: {formData.note}</h5>
+      <h5>Note: {formData.note}</h5>
     </div>
   );
-};
+}
 
 export default App;

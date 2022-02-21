50389139aca961cad81384d9a03162217d187f02
nf2000 <nimraf@chillibean.com>
2022-02-21 10:43:52 +0000

    formatting with pretiter

--- nims-notes-things/src/TextArea.js
+++ nims-notes-things/src/TextArea.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const TextArea = (props) =>{
-  const {name, value, placeholder, onChange, onBlur } = props;  
+const TextArea = (props) => {
+  const { name, value, placeholder, onChange, onBlur } = props;
 
-  return(
+  return (
     <textarea
       name={name}
-      value={value} 
-      onChange={onChange} 
+      value={value}
+      onChange={onChange}
       placeholder={placeholder}
       onBlur={onBlur}
     />
-  )
-}
+  );
+};
 export default TextArea;

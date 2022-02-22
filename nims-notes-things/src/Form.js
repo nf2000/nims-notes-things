0612ee4349916eb3f7d1aaa4c61d56ffcdabbada
nf2000 <nimraf@chillibean.com>
2022-02-22 09:59:20 +0000

    Indentation added

--- nims-notes-things/src/Form.js
+++ nims-notes-things/src/Form.js
@@ -30,41 +30,40 @@ const Form = (props) => {
         [key]: value === "",
       };
     });
-    //console.log(empty);
   };
 
   return (
     <form onSubmit>
-    <div>
-    {empty.name && <p>This field is required</p>}
-      <Input
-        name="name"
-        placeholder="Please enter Your name"
-        onChange={(e) => handleOnChange(e, "name")}
-        onBlur={(e) => handleOnBlur(e.target.value, "name")}
-        value={formData.name}
-      />
-      <h5>Name: {formData.name}</h5>
-      {empty.date && <p>This field is required</p>}
-      <Input
-        name="date"
-        placeholder="Please enter the date"
-        onChange={(e) => handleOnChange(e, "date")}
-        onBlur={(e) => handleOnBlur(e.target.value, "date")}
-        value={formData.date}
-      />
-      <h5>Date: {formData.date}</h5>
-      {empty.note && <p>This field is required</p>}
-      <TextArea
-        name="note"
-        placeholder="Please enter Your Note"
-        onChange={(e) => handleOnChange(e, "note")}
-        onBlur={(e) => handleOnBlur(e.target.value, "note")}
-        value={formData.note}
-      />
-      <h5>Note: {formData.note}</h5>
-    </div>
-    <button type="submit" />
+      <div>
+        {empty.name && <p>This field is required</p>}
+        <Input
+          name="name"
+          placeholder="Please enter Your name"
+          onChange={(e) => handleOnChange(e, "name")}
+          onBlur={(e) => handleOnBlur(e.target.value, "name")}
+          value={formData.name}
+        />
+        <h5>Name: {formData.name}</h5>
+        {empty.date && <p>This field is required</p>}
+        <Input
+          name="date"
+          placeholder="Please enter the date"
+          onChange={(e) => handleOnChange(e, "date")}
+          onBlur={(e) => handleOnBlur(e.target.value, "date")}
+          value={formData.date}
+        />
+        <h5>Date: {formData.date}</h5>
+        {empty.note && <p>This field is required</p>}
+        <TextArea
+          name="note"
+          placeholder="Please enter Your Note"
+          onChange={(e) => handleOnChange(e, "note")}
+          onBlur={(e) => handleOnBlur(e.target.value, "note")}
+          value={formData.note}
+        />
+        <h5>Note: {formData.note}</h5>
+      </div>
+      <button type="submit" />
     </form>
   );
 };

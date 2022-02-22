2b7159bbe2821cf9d34962fd87c65b7a06e336ba
nf2000 <nimraf@chillibean.com>
2022-02-22 12:17:10 +0000

    Added new div for the app header

--- nims-notes-things/src/App.js
+++ nims-notes-things/src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
+// import pencil from './penLogo.png';
 import "./App.css";
 import Form from "./Form";
 
 function App() {
   return (
     <div className="App">
+      <div className="App-header">
       <h1> Nims Notes Things </h1>
+      </div>
       <Form />
     </div>
   );

a74f520fc85127660c85984da07e2ef674036148
nf2000 <nimraf@chillibean.com>
2022-02-28 15:45:19 +0000

    Pictures and positioning

--- nims-notes-things/src/components/notes.js
+++ nims-notes-things/src/components/notes.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
+import Pin from "../pin.jpeg";
+import Board from "../corkboard.jpeg";
 
 function Notes() {
   const notes = useSelector((state) => {
@@ -13,9 +15,12 @@ function Notes() {
         return (
           <div>
             <Note>
-              <p>{n.name}</p>
-              <p>{n.date}</p>
-              <p>{n.note}</p>
+              <img src={Pin} height={20} alt="" />
+              <Header>
+                <Name>{n.name}</Name>
+                <Date>{n.date}</Date>
+              </Header>
+              <Content>{n.note}</Content>
             </Note>
           </div>
         );
@@ -26,12 +31,42 @@ function Notes() {
 export default Notes;
 
 const Note = styled.div`
-  display: grid;
   flex-wrap: wrap;
   background-color: #e7f3fe;
   width: 200px;
   margin: 10px;
   padding: 20px;
   float: left;
+  box-shadow:  0 0 5px blue;
+  }
+`;
+
+const BoardBack = styled.div`
+background: url("../corkboard.jpeg");
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex: 1;
+  font-style: italic;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  font-weight: bold;
+  }
+`;
+
+const Date = styled.div`
+
+  }
+`;
+
+const Name = styled.div`
+
+  }
+`;
+
+const Content = styled.div`
+  text-align: left;
+  font-size: 17px;
   }
 `;

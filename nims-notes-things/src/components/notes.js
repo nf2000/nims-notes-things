b5f352154bc8f091e32e779d4af26623864c11fa
nf2000 <nimraf@chillibean.com>
2022-03-02 16:18:35 +0000

    adding unique key id to note

--- nims-notes-things/src/components/notes.js
+++ nims-notes-things/src/components/notes.js
@@ -12,17 +12,15 @@ function Notes() {
     <div>
       {notes.map((note, index) => {
         return (
-          <div>
-            <Note>
-              <PinImage src={Pin} height={20} alt=""></PinImage>
-              <NoteNumber> {index + 1}</NoteNumber>
-              <Header>
-                <p>{note.name}</p>
-                <p>{note.date}</p>
-              </Header>
-              <Content>{note.note}</Content>
-            </Note>
-          </div>
+          <Note key={note.id}>
+            <PinImage src={Pin} height={20} alt=""></PinImage>
+            <NoteNumber> {index + 1}</NoteNumber>
+            <Header>
+              <p>{note.name}</p>
+              <p>{note.date}</p>
+            </Header>
+            <Content>{note.note}</Content>
+          </Note>
         );
       })}
     </div>
